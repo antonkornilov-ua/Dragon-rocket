647386ba9cfd512480af062c9d16c25ca666a4a4
antonkornilov-ua <83725472+antonkornilov-ua@users.noreply.github.com>
2022-10-03 14:28:43 +0300

    v.1.0.1
    Refactoring

--- js/app.js
+++ js/app.js
@@ -1,16 +1,14 @@
 // ==========================
 // Hide loader section \\
-
 setTimeout(() => {
     const loadSection = document.querySelector('.load_screen');
     loadSection.style.transition = 'opacity ' + 3 + 's';
     loadSection.style.opacity = 0;
     loadSection.addEventListener('transitionend', function () {
-        console.log("event has been ended");
-        loadSection.style.display = "none";
-    })
+        console.log('event has been ended');
+        loadSection.style.display = 'none';
+    });
 }, 4000);
-
 // ==========================
 // Show main info \\
 setTimeout(() => {
@@ -18,13 +16,8 @@ setTimeout(() => {
     console.log('Data has been loaded');
     mainSection.style.display = 'flex';
 }, 7300);
-
-
 // ==========================
 // Get info from server \\
-
-// variables
-
 const url = 'https://api.spacexdata.com/v4/dragons/5e9d058759b1ff74a7ad5f8f';
 const nameDragon = document.querySelector('.item1_info');
 const typeDragon = document.querySelector('.item2_info');
@@ -37,8 +30,8 @@ const volPayloadDragon = document.querySelector('.item8_info');
 const orbitDragon = document.querySelector('.item9_info');
 const wikiLinkDragon = document.querySelector('.wiki_link');
 const aboutDragon = document.querySelector('.about');
-
-
+// ==========================
+// Parse info from server \\
 document.addEventListener('DOMContentLoaded', function () {
     fetch(url)
         .then((response) => {
@@ -49,7 +42,7 @@ document.addEventListener('DOMContentLoaded', function () {
         })
         .then((json) => {
             nameDragon.innerHTML = json.name;
-            typeDragon.innerHTML = json.type
+            typeDragon.innerHTML = json.type;
             flyDragon.innerHTML = json.first_flight;
             heightDragon.innerHTML = json.height_w_trunk.meters + ' m';
             massDragon.innerHTML = json.dry_mass_kg + ' kg';
@@ -61,13 +54,16 @@ document.addEventListener('DOMContentLoaded', function () {
             aboutDragon.innerHTML = json.description;
         });
 });
+// ==========================
+// Parse img from server \\
 fetch(url)
     .then((r) => r.json())
     .then((d) => {
         document.querySelectorAll('.image_dragon').forEach((el, i) => (el.src = d.flickr_images[i]));
         initCarousel();
     });
-
+// ==========================
+// Swiper \\
 function initCarousel() {
     const swiper = new Swiper('.swiper', {
         direction: 'horizontal',
@@ -83,4 +79,3 @@ function initCarousel() {
         mousewheel: true,
     });
 }
-
